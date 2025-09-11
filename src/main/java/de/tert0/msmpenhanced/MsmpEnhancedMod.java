a20c99777bc4449332618f524de4ff4e453afa6a
Tert0 <tert0byte@gmail.com>
2025-09-11 23:38:41 +0200

    initial commit

---
+++ src/main/java/de/tert0/msmpenhanced/MsmpEnhancedMod.java
@@ -0,0 +1,9 @@
+package de.tert0.msmpenhanced;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MsmpEnhancedMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
