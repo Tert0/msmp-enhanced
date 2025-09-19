62144eb607543a76fd2f8dd660f3ec64cf21ca87
Tert0 <tert0byte@gmail.com>
2025-09-19 22:12:30 +0200

    1.21.9-pre2

--- src/main/java/de/tert0/msmpenhanced/management/ModRpcSchema.java
+++ src/main/java/de/tert0/msmpenhanced/management/ModRpcSchema.java
@@ -8,15 +8,15 @@ public class ModRpcSchema {
     public static final RpcSchemaEntry COMMAND_SUGGESTION = RpcSchemaAccessor.registerEntry(
             "msmp_enhanced_command_suggestion",
             RpcSchema.ofObject()
-                    .withProperty("text", RpcSchema.STRING.toItemSchema())
-                    .withProperty("tooltip", RpcSchema.ofObject().toItemSchema()) // TODO optional
+                    .withProperty("text", RpcSchema.STRING)
+                    .withProperty("tooltip", RpcSchema.ofObject()) // TODO optional
     );
 
     public static final RpcSchemaEntry COMMAND_SUGGESTIONS = RpcSchemaAccessor.registerEntry(
             "msmp_enhanced_command_suggestions",
             RpcSchema.ofObject()
-                    .withProperty("start", RpcSchema.INTEGER.toItemSchema())
-                    .withProperty("end", RpcSchema.INTEGER.toItemSchema())
-                    .withProperty("suggestions", ModRpcSchema.COMMAND_SUGGESTION.ref().toItemSchema())
+                    .withProperty("start", RpcSchema.INTEGER)
+                    .withProperty("end", RpcSchema.INTEGER)
+                    .withProperty("suggestions", ModRpcSchema.COMMAND_SUGGESTION.ref())
     );
 }
